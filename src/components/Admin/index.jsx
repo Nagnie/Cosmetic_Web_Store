c545c7e0a0c106f3ad44b89cbcc6389d316c1978
22120225 <tranthaongan7epct@gmail.com>
2025-02-26 02:37:21 +0700

    divide backend & frontend

--- src/components/Admin/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as Product } from "./Product/Product.jsx";
-export { default as Brand } from "./Brand/Brand.jsx";
-export { default as Category } from "./Category/Category.jsx";
--- src/components/Layout/index.jsx
+++
@@ -1,2 +0,0 @@
-export { default as AdminLayout } from "./AdminLayout/AdminLayout.jsx";
-export { default as GuestLayout } from "./GuestLayout/GuestLayout.jsx";
