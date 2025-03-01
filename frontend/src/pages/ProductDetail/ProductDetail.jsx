04bb0b640b74ea1eb06989ffc5d11518aeca560a
22120217 <hoanglenam0905@gmail.com>
2025-03-01 16:41:10 +0700

    feat: add transition effect to Product Image Gallery
    - **Enhancements**:
      - Added transition effects to `ProductImageGallery.jsx` for smoother image changes.

--- frontend/src/pages/ProductDetail/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail/ProductDetail.jsx
@@ -3,7 +3,7 @@ import ProductImageGallery from "./components/ProductImageGallery";
 const ProductDetail = () => {
   return (
     <div>
-      <div className="mt-35 grid w-full grid-cols-2 gap-4 pt-10">
+      <div className="mt-35 grid w-full grid-cols-1 gap-4 pt-10 md:grid-cols-2">
         <div>
           <ProductImageGallery />
         </div>
