1db1f0ea46ac0727aeb8bdb2ec8bf80a938db015
22120217 <hoanglenam0905@gmail.com>
2025-03-08 21:17:50 +0700

    feat: Implement Form Persistence and Location Service
    - **useFormPersistence.jsx**: Custom hook to persist and restore form data from `localStorage`.
    - **LocationService.js**: API service to fetch city, district, and ward data.
    - **CheckoutCustomerInfo.jsx**: New component for customer details in checkout.
    - **vite.config.js**: Added alias for `services/` directory.
    - **CheckoutPage.jsx**: Integrated form persistence and location service.
    - **jsconfig.json**: Updated configuration for aliasing.

--- frontend/src/pages/Checkout/CheckoutPage.jsx
+++ frontend/src/pages/Checkout/CheckoutPage.jsx
@@ -1,4 +1,15 @@
+import { CheckoutCustomerInfo } from "./components";
+
 const CheckoutPage = () => {
-  return <div className="mx-auto mt-35 mb-4 pt-10 lg:px-4">CheckoutPage</div>;
+  return (
+    <div className="mx-auto mt-35 mb-4 pt-10 lg:px-4">
+      <div className="grid grid-cols-1 gap-1 text-left lg:grid-cols-2 lg:gap-8">
+        {/* Left */}
+        <CheckoutCustomerInfo />
+        {/* Right */}
+        <div></div>
+      </div>
+    </div>
+  );
 };
 export default CheckoutPage;
---
+++ frontend/src/pages/Checkout/components/index.js
@@ -0,0 +1,1 @@
+export { default as CheckoutCustomerInfo } from "./CheckoutCustomerInfo";
