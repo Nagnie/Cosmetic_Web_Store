7513259aefe5b4c41246ea1967c7860a1701d61b
22120198 <trinhnguyenluong182306@gmail.com>
2025-03-10 09:17:12 +0700

    Add product pagination

--- backend/src/module/cart/setup-redis.md
+++ backend/src/module/cart/setup-redis.md
@@ -1,15 +1,13 @@
 # HƯỚNG DẪN CÀI ĐẶT REDIS LOCALHOST
 
-Trên Ubuntu VPS
-
+## Trên Ubuntu VPS
 Chạy các lệnh sau để cài đặt Redis:
 
 sudo apt update && sudo apt install redis -y
 sudo systemctl start redis
 sudo systemctl enable redis-server
 
-Kiểm tra Redis đang chạy:
-
+## Kiểm tra Redis đang chạy:
 redis-cli ping
 
 Nếu kết quả trả về PONG, Redis đã hoạt động thành công.
--- backend/src/module/order/dto/create-order.dto.ts
+++ backend/src/module/order/dto/create-order.dto.ts
@@ -1,1 +1,11 @@
-export class CreateOrderDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateOrderDto {
+    @ApiProperty({ example: 1, description: `Product ID` })
+    @IsNotEmpty({ message: `Product ID must not be empty` })
+    @IsString({ message: `Product ID must be a number` })
+    name: string;
+
+    
+}
